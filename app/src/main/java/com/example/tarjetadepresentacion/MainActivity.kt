d3fda081262df20f987d11e54af3d465f98b16d3
nebur <rubenarranz04@gmail.com>
2023-10-19 16:45:17 +0200

    Versión final y funcional de la tarjeta de presentación + arreglo del crash y alineamiento de los elementos

--- app/src/main/java/com/example/tarjetadepresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetadepresentacion/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.tarjetadepresentacion
 
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -23,13 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Caja()
+                    ProductoFinal()
                 }
             }
         }
@@ -56,50 +53,100 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Contenido() {
-    Column {
-
-    }
-}
-
-@Composable
 fun PresentacionHorizontal(modifier: Modifier = Modifier) {
     val imagen = painterResource(R.drawable.android_logo)
+    val fotoMia = painterResource(R.drawable.foto_de_mi)
     val iconoTelefono = painterResource(R.drawable.telefono)
     val iconoMail = painterResource(R.drawable.mail)
-    Row(
+    val phone = "+34 634 50 62 29"
+    val context = LocalContext.current
+    Column(
         Modifier
             .background(color = Color.Gray)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Start
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Image(
-            painter = imagen,
-            contentDescription = null,
-            contentScale = ContentScale.Inside,
-            modifier = Modifier
-                .padding(top = 24.dp, start = 16.dp, end = 16.dp)
-                .fillMaxWidth(0.15f)
-                .background(Color(2, 57, 106))
-                .border(BorderStroke(2.dp, Color.Black))
-        )
-        Column {
-            Text(
-                text = "Rubén Arranz",
-                fontSize = 50.sp,
-                color = Color.White,
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = imagen,
+                contentDescription = null,
+                contentScale = ContentScale.Inside,
                 modifier = Modifier
-                    .padding(top = 16.dp)
+                    .padding(top = 24.dp, start = 16.dp, end = 16.dp)
+                    .fillMaxWidth(0.15f)
+                    .background(Color(2, 57, 106))
+                    .border(BorderStroke(2.dp, Color.Black))
             )
-            Text(
-                text = "Estudiante de DAM",
-                color = Color.Green,
-                fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+            Column {
+                Text(
+                    text = "Rubén Arranz",
+                    fontSize = 50.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                )
+                Text(
+                    text = "Estudiante de DAM",
+                    color = Color.Green,
+                    fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                    modifier = Modifier
+                        .align(alignment = Alignment.End)
+                )
+            }
+            Image(
+                painter = fotoMia,
+                contentDescription = null,
                 modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.4f)
+
             )
         }
+        Row (
+            Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(bottom = 40.dp)
+        ){
+            Image(
+                painter = iconoTelefono,
+                contentDescription = null,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
+            )
+            TextButton(
+                onClick = {
+                    val textPhone = "tel: $phone"
+                    val intent = Intent(Intent.ACTION_DIAL)
+                    intent.data = Uri.parse(textPhone)
+                    startActivity(context, intent, null)
+                },
+                Modifier.align(alignment = Alignment.Bottom)
+            ) {
+                Text(
+                    text = phone,
+                    color = Color(0, 68, 129),
+                    modifier = Modifier
+                )
+            }
+            Image(
+                painter = iconoMail,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 12.dp, start = 16.dp)
+                    .align(alignment = Alignment.CenterVertically)
+            )
+            Text(
+                text = "rubenarranz04@gmail.com",
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
+            )
 
+        }
     }
 }
 
@@ -108,16 +155,16 @@ fun PresentacionVertical(modifier: Modifier = Modifier) {
     val imagen = painterResource(R.drawable.android_logo)
     val iconoTelefono = painterResource(R.drawable.telefono)
     val iconoMail = painterResource(R.drawable.mail)
-    val uri = "+34 677 38 77 47"
+    val phone = "+34 634 50 62 29"
     val context = LocalContext.current
-    val intent = Intent(Intent.ACTION_DIAL)
-    Column (
+    Column(
         Modifier
             .background(color = Color.Gray)
             .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Image(
@@ -146,30 +193,33 @@ fun PresentacionVertical(modifier: Modifier = Modifier) {
                     .align(alignment = Alignment.CenterHorizontally)
             )
         }
-        Column (
+        Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Bottom
-        ){
+        ) {
             Row(
                 Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
+                    .align(alignment = Alignment.Start)
             ) {
                 Image(
                     painter = iconoTelefono,
                     contentDescription = null,
                     modifier = Modifier
-                        .padding(end = 5.dp)
+                        .align(alignment = Alignment.CenterVertically)
                 )
                 TextButton(
                     onClick = {
-                        intent.data = Uri.parse(uri)
+                        val textPhone = "tel: $phone"
+                        val intent = Intent(Intent.ACTION_DIAL)
+                        intent.data = Uri.parse(textPhone)
                         startActivity(context, intent, null)
                     },
                     Modifier.align(alignment = Alignment.CenterVertically)
                 ) {
                     Text(
-                        text = uri,
+                        text = phone,
                         color = Color(0, 68, 129),
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
                     )
                 }
             }
@@ -181,7 +231,7 @@ fun PresentacionVertical(modifier: Modifier = Modifier) {
                     painter = iconoMail,
                     contentDescription = null,
                     modifier = Modifier
-                        .padding(end = 5.dp)
+                        .padding(end = 12.dp)
                 )
                 Text(
                     text = "rubenarranz04@gmail.com",
@@ -190,19 +240,11 @@ fun PresentacionVertical(modifier: Modifier = Modifier) {
             }
         }
     }
-    }
-/*
-            val uri = "+34 677 38 77 47"
-            val context = LocalContext.current
-            val intent = Intent(Intent.ACTION_DIAL)
-            intent.data = Uri.parse(uri)
-            startActivity(intent)
-     */
 
-
+}
 
 @Composable
-fun Caja() {
+fun ProductoFinal() {
     val imagen = painterResource(R.drawable.android_logo)
     BoxWithConstraints(Modifier.fillMaxSize()) {
         if (maxWidth < 500.dp) {
@@ -214,24 +256,10 @@ fun Caja() {
 }
 
 
-@Composable
-fun Info() {
-
-}
-
-@Preview
-@Composable
-fun PreviewHorizontal() {
-    TarjetaDePresentacionTheme {
-        PresentacionHorizontal()
-    }
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun PreviewVertical() {
     TarjetaDePresentacionTheme {
-        Caja()
+        PresentacionVertical()
     }
 }
